8698bd18c6afa03caee1d11ffad77d00bc80d83c
venkat3697 <naveen.run17@gmail.com>
2024-12-08 21:42:44 +0530

    components

---
+++ src/Views/Intro/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/Header";
+import './intro.scss'
+
+const Intro=()=>{
+    return <div  className="intro-container">
+    <Header/>
+    {/* <div className="about-section">
+        check
+    </div> */}
+   </div>
+}
+
+
+export default Intro
